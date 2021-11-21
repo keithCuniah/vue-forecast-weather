f3721e918745eebb190b79e8b030b6ce25472758
Keith Cuniah <88380932+keithCuniah@users.noreply.github.com>
2021-11-21 13:33:29 +0100

    server side : improver api informations & cleanning code

--- server/app.js
+++ server/app.js
@@ -3,10 +3,10 @@ const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const countryRouter = express.Router();
+const weatherForecastRouter = express.Router();
 
 // Route for countries
-countryRouter.route('/countries').get(async (req, res) => {
+weatherForecastRouter.route('/countries').get(async (req, res) => {
   console.log('axios call for get countries');
   const url = 'https://countriesnow.space/api/v0.1/countries/flag/unicode';
   try {
@@ -19,10 +19,10 @@ countryRouter.route('/countries').get(async (req, res) => {
     console.log(` Call in error : ${err}`);
   }
 });
-app.use('/api', countryRouter);
+app.use('/api', weatherForecastRouter);
 
 // Route for cities of one country
-countryRouter.route('/countries/:country').get(async (req, res) => {
+weatherForecastRouter.route('/countries/:country').get(async (req, res) => {
   const url = 'https://countriesnow.space/api/v0.1/countries/cities';
   const data = { country: req.params.country.toLowerCase() };
 
@@ -39,10 +39,10 @@ countryRouter.route('/countries/:country').get(async (req, res) => {
     console.log(` Call in error : ${err}`);
   }
 });
-app.use('/api', countryRouter);
+app.use('/api', weatherForecastRouter);
 
 // Route for current and forecast weather of one country or one city
-countryRouter
+weatherForecastRouter
   .route('/:weatherOrForecast/:countryOrCity')
   .get(async (req, res) => {
     const apiKey = 'c4c8cb4635149c853f5fe04023add8c5';
@@ -84,10 +84,38 @@ countryRouter
       console.log(` Call in error : ${err}`);
     }
   });
-app.use('/api', countryRouter);
+app.use('/api', weatherForecastRouter);
 
+// Endpoint
 app.get('/', (req, res) => {
-  res.send('Welcome to my Weather Forecast server API');
+  res.send(`<div>
+              <h1>Welcome to my Weather Forecast server API</h1>
+              <p>Go to /api</p>
+            </div>`);
+});
+
+app.get('/api', (req, res) => {
+  res.send(`<div>
+              <h1>Welcome to my Weather Forecast server API</h1>
+              <p>API paths :</p>
+              <ul>
+                <li>To get all countries and flags
+                  <b>/api/countries</b>
+                </li>
+
+                <li>To get all cities from this country
+                  <b>/api/countries/{country_name}</b>
+                </li>
+
+                <li>To get the current weather of a specific location (country or city)
+                  <b>/api/weather/{country_name or city_name}</b>
+                </li>
+                
+                <li>To get the forecast of a specific location (country or city)
+                  <b>/api/forecasst/{country_name or city_name}</b>
+                </li>
+              </ul>
+            </div>`);
 });
 
 app.listen(port, () => {
